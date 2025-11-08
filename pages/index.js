8079d3f420145892088b1804c4c18c90d5d95db4
Edgar Gillies <info@upflexdigital.com>
2025-11-07 23:11:54 -0800

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
+      <h1>Test Website</h1>
+      <p>This is a test website created by the bulk website generator integration test.</p>
+      <p>If you can see this, the end-to-end integration is working correctly!</p>
+    </div>
+  );
+}
